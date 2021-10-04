fde39e7d7218abbbf6ec78fa16f8288f6cf11f88
brentleejohnson <brentleejohnson73@gmail.com>
2021-10-04 09:00:51 +0200

    Token works

--- middleware/auth.js
+++ middleware/auth.js
@@ -13,7 +13,7 @@ module.exports = function (req, res, next) {
   try {
     const decoded = jwt.verify(token, config.get("jwtSecret"));
 
-    req.user = deocded.user;
+    req.user = decoded.user;
     next();
   } catch (err) {
     res.status(401).json({ msg: "Token is not valid" });
